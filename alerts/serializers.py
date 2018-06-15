ef73868b0c67ff4e974e39294fa6ad19178adf90
leiserfg <leiserfg@gmail.com>
2018-06-14 21:36:15 -0400

    Add extra fields to serializer

--- alerts/serializers.py
+++ alerts/serializers.py
@@ -5,6 +5,9 @@ from .models import Alert
 
 class AlertSerializer(serializers.ModelSerializer):
     owner = serializers.StringRelatedField(read_only=True)
+    frequency_name = serializers.CharField(
+        source='get_frequency_display', read_only=True)
+    email = serializers.EmailField(write_only=True)
 
     class Meta:
         model = Alert
