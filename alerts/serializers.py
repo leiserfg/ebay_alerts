7d6f1560dbe9cef4370b8adaa95d671e6acdaa70
leiserfg <leiserfg@gmail.com>
2018-06-14 22:36:08 -0400

    Reimplement create to CreateSerializer

--- alerts/serializers.py
+++ alerts/serializers.py
@@ -7,8 +7,22 @@ class AlertSerializer(serializers.ModelSerializer):
     owner = serializers.StringRelatedField(read_only=True)
     frequency_name = serializers.CharField(
         source='get_frequency_display', read_only=True)
-    email = serializers.EmailField(write_only=True)
 
     class Meta:
         model = Alert
         fields = '__all__'
+
+
+class CreateAlertSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(write_only=True)
+    frequency_name = serializers.CharField(
+        source='get_frequency_display', read_only=True)
+
+    class Meta:
+        model = Alert
+        fields = ('id', 'email', 'search_terms', 'frequency', 'frequency_name')
+
+    def create(self, validated_data):
+        data = validated_data.copy()
+        email = data.pop('email')
+        return Alert.create_with_email(email, **data)
