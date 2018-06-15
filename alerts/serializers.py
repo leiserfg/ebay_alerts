80a4d34aacb18cc079add7358989b692aadef5a9
leiserfg <leiserfg@gmail.com>
2018-06-14 20:39:37 -0400

    Add AlertSerializer

---
+++ alerts/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from .models import Alert
+
+
+class AlertSerializer(serializer.ModelSerializer):
+    owner = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Alert
+        fields = '__all__'
