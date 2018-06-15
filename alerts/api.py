f45f9e9873c7d847e90aa5cfdd7937e119ae87b7
leiserfg <leiserfg@gmail.com>
2018-06-14 23:20:25 -0400

    Add dynamic queryset and query_parameter to AlertView

--- alerts/api.py
+++ alerts/api.py
@@ -1,4 +1,7 @@
 from django.db import transaction
+from django.utils.decorators import method_decorator
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
 from rest_framework.routers import SimpleRouter
 from rest_framework.viewsets import ModelViewSet
@@ -9,10 +12,19 @@ from .utils import register_viewset
 
 router = SimpleRouter()
 
+owner_param = openapi.Parameter('owner', in_=openapi.IN_QUERY, description='Owner of the alert',
+                                type=openapi.TYPE_STRING)
 
-@register_viewset(router, 'alerts')
+
+@method_decorator(name='list', decorator=swagger_auto_schema(manual_parameters=[owner_param]))
+@register_viewset(router, 'alerts', 'alert')
 class AlertView(ModelViewSet):
-    queryset = Alert.objects.all()
+    def get_queryset(self):
+        queryset = Alert.objects.all()
+        owner = self.request.query_params.get('owner', None)
+        if owner is not None:
+            return queryset.filter(owner__email=owner)
+        return queryset.none()
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
