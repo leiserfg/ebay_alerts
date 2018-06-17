419ff88e55699a13bd5d4d924fd785aac7662204
leiserfg <leiserfg@gmail.com>
2018-06-17 11:49:58 -0400

    Implements process_alert task

--- alerts/tasks/process_alerts.py
+++ alerts/tasks/process_alerts.py
@@ -2,11 +2,14 @@ from huey import crontab
 from huey.contrib.djhuey import db_periodic_task, db_task, task
 
 from ..models import Alert
+from ..utils import search_on_ebay
 
 
 @task()
-def process_alert(alert):
-    print(alert.owner.email)
+def process_alert(alert: Alert):
+    from ..emails import send_alert  # here for avoid cyclic import
+    items = search_on_ebay(alert.search_terms)
+    send_alert(alert, items)
 
 
 @db_periodic_task(crontab(minute='*'))
