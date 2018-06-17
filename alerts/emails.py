4a74983075bc1189d7d0846e06a0330185489822
leiserfg <leiserfg@gmail.com>
2018-06-17 19:43:29 -0400

    Add _manage_action [WIP]

--- alerts/emails.py
+++ alerts/emails.py
@@ -15,6 +15,10 @@ def _unsuscribe_action(alert: Alert):
     return absolute_reverse('alert-unsuscribe', args=[alert.id])
 
 
+def _manager_action(customer: Customer):
+    return absolute_reverse('customer-detail', args=[customer.id])
+
+
 def subscription(alert: Alert):
     email = alert.owner.email
     frequency = alert.get_frequency_display()
@@ -25,7 +29,9 @@ def subscription(alert: Alert):
         frequency=frequency,
         search_terms=search_terms,
         actions=[{'text': 'Confirm Subscription',
-                  'link': _subscribe_action(alert)}]
+                  'link': _subscribe_action(alert)},
+                 {'text': 'Manage your notifications',
+                  'link': _manager_action(alert.owner)}]
     )
     template_txt = loader.get_template('alerts/subscription.txt')
     template_html = loader.get_template('alerts/subscription.html')
@@ -52,7 +58,9 @@ def send_alert(alert: Alert, items):
         search_terms=search_terms,
         items=items,
         actions=[{'text': 'Unsuscribe',
-                  'link': _unsuscribe_action(alert)}]
+                  'link': _unsuscribe_action(alert)},
+                 {'text': 'Manage your notifications',
+                  'link': _manager_action(alert.owner)}]
     )
     template_txt = loader.get_template('alerts/alert.txt')
     template_html = loader.get_template('alerts/alert.html')
